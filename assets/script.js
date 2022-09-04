d624cef363712239b03fbe6ea060fe28f7887899
Borthvader <104023232+borthvader@users.noreply.github.com>
2022-09-04 14:01:24 -0400

    Added Local storage return

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
-
+// Function runs on page load to display time and fecth objects from local storage
 window.onload = function() {
+  var currentBlocks = fetchCurrentBlocks;
   var currentTime = moment();
 
   displayDate(currentTime);
@@ -11,8 +12,16 @@ window.onload = function() {
     });
     setText(currentBlocks);
 }
-
+// displays current date and time for reference
 function displayDate(currentTime){
   document.getElementById('currentDay')
   .textContent = currentTime.format ('dddd, MMMM Do, YYYY, h:mm a');
 }
+// returns saved objects from local storage
+function fetchCurrentBlocks (){
+  var currentBlocks = localstorage.getItem('timeblocksObj');
+  return currentBlocks ? JSON.parse(currentBlocks) : [];
+}
+
+
+
